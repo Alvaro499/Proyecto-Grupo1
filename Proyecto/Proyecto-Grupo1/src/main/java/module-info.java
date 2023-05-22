f619e9a29ecbc6a7117a3c3e11ef38efbbcf55bb
Alvaro499 <71684805+Alvaro499@users.noreply.github.com>
2023-05-21 18:55:22 -0600

    Add files via upload

---
+++ Proyecto/Proyecto-Grupo1/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ucr.proyecto.proyectogrupo1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ucr.proyecto.proyectogrupo1 to javafx.fxml;
+    exports ucr.proyecto.proyectogrupo1;
+}
