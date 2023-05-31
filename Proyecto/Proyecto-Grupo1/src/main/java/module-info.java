2af02a712ba055f1b0ae1193cd59a39bba6cb6af
Lani6 <117134960+Lani6@users.noreply.github.com>
2023-05-30 18:58:22 -0600

    Update module-info.java

--- Proyecto/Proyecto-Grupo1/src/main/java/module-info.java
+++ Proyecto/Proyecto-Grupo1/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module ucr.proyecto.proyectogrupo1 {
 
     opens ucr.proyecto.proyectogrupo1 to javafx.fxml;
     exports ucr.proyecto.proyectogrupo1;
+    exports ucr.proyecto.proyectogrupo1.controller;
+    opens ucr.proyecto.proyectogrupo1.controller to javafx.fxml;
 }
